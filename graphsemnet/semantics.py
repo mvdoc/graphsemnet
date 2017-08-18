e7ad66a181a81682158680e762ab2917190717ee
Beau Sievers <beau@beausievers.com>
2017-08-18 17:11:27 -0400

    split revised model into own files, added demo notebook

---
+++ graphsemnet/semantics.py
@@ -0,0 +1,36 @@
+import random
+from scipy.spatial.distance import pdist
+import numpy as np
+
+
+def connected_word_list(seed_word, keyed_vectors, n_iterations=5):
+    all_words = [seed_word]
+    for i in range(n_iterations):
+        seed_word = random.sample(all_words, 1)
+        nearest_neighbors = list(
+            np.array(
+                keyed_vectors.most_similar(seed_word)
+            )[:, 0]
+        )
+        all_words += nearest_neighbors
+    return all_words
+
+
+def semantic_dsm(word_list, keyed_vectors):
+    vectors = np.array([keyed_vectors.word_vec(word) for word in word_list])
+    dsm = np.clip(pdist(vectors, metric='cosine'), 0, 1)
+    return dsm
+
+
+def semantic_dsm_safe(word_list, keyed_vectors):
+    vectors = []
+    labels = []
+    for word in word_list:
+        try:
+            vectors.append(keyed_vectors.word_vec(word))
+            labels.append(word)
+        except:
+            pass
+    vectors = np.array(vectors)
+    matrix = pdist(vectors, metric='cosine')
+    return (matrix, labels)
