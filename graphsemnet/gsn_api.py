a3deffd4db81ab7d47d65063e3e12d1326f2afd9
Beau Sievers <beau@beausievers.com>
2017-10-31 15:40:18 -0400

    move funcs from mvdoc in but still need to make them conform

--- graphsemnet/gsn_api.py
+++ graphsemnet/gsn_api.py
@@ -24,6 +24,10 @@ def operate_recur(graph, activations, xcal, decay):
     return reweight_recur(graph, new_activations, xcal)
 
 
+def operate_depth(graph, activations, xcal, decay):
+    pass
+
+
 def propagate_recur(graph, activations, xcal, decay=0.8, new_adj=None,
                     debug=False):
     """Fire together?
@@ -78,3 +82,63 @@ def reweight_recur(graph, activations, xcal, debug=False):
     np.fill_diagonal(new_graph.adj, 0)
     new_graph.adj = np.clip(new_graph.adj, 0, 1)
     return new_graph
+
+
+def compute_adjacency(W):
+    """Given a weight matrix W, return an adjacency matrix"""
+    A = W.copy()
+    A[A > 0.] = 1.
+    return A
+
+
+def rect(x):
+    """ReLU function"""
+    return np.clip(x, 0, 1.)
+
+
+def spread_activation(W0, ACT0, nmph, gamma=0.8, alpha=0.5, lambda_=1, d=3):
+    """Spread activation with NMPH on a graph with initial weight W0,
+    and activation ACT0
+
+    Arguments
+    ---------
+    W0 : array (n_nodes, n_nodes)
+        initial weights of the graph
+    ACT0 : array (1, n_nodes)
+        row array of initial activations
+    npmh : function [0, 1] -> [-1, 1]
+        nmph function, generated with `compute_nmph`
+    gamma : float [0, 1]
+        decay parameter
+    alpha : float [0, 1]
+        learning parameter
+    lambda_ : float [0, 1]
+        decay parameter for past activations
+    d : int
+        how far the activation is allowed to spread
+
+    Returns
+    -------
+    Ws : list of arrays (n_nodes, n_nodes)
+    ACT : list of arrays
+        activations for every depth
+    """
+    # initialize values
+    assert (ACT0.ndim == 2 and (ACT0.shape[0] <= ACT0.shape[1]))
+    A = compute_adjacency(W0)
+    Ws = [W0]
+    ACT = [ACT0]
+    dACT = np.zeros(ACT0.shape)
+
+    # loop
+    for i in range(1, d):
+        # update W
+        W_i = rect(Ws[-1] + alpha * nmph(ACT[-1]).T * A)
+        Ws.append(W_i)
+        # update ACT
+        dACT += gamma ** i * np.dot(ACT[-1], np.multiply.reduce(Ws))
+        ACT_ = rect((lambda_**i) * ACT[-1] + dACT)
+        ACT.append(ACT_)
+        #print("Loop {0}: len(Ws): {1}\tlen(ACT): {2}".format(i, len(Ws), len(ACT)))
+    return Ws, ACT
+
